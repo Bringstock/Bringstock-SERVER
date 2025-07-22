92b2f7b4636ccba5a73459c0e51fd90a9e89bb7e
허세진 <heosejin222@gmail.com>
2025-07-22 19:50:26 +0900

    init

---
+++ src/main/java/Majorpiece/bringstock/BringstockApplication.java
@@ -0,0 +1,13 @@
+package Majorpiece.bringstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BringstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BringstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Majorpiece/bringstock/BringstockApplicationTests.java
@@ -0,0 +1,13 @@
+package Majorpiece.bringstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BringstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
