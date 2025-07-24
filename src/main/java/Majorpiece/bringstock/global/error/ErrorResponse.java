38de781853d6a28ff97fa89f2bb9f18b1765bdb7
허세진 <heosejin222@gmail.com>
2025-07-24 18:09:10 +0900

    feat: global 에러 처리 구조 구현

---
+++ src/main/java/Majorpiece/bringstock/global/error/ErrorResponse.java
@@ -0,0 +1,16 @@
+package Majorpiece.bringstock.global.error;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private final int status;
+    private final String message;
+
+    @Builder
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
