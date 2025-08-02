923d8dae3f78ae8e5e9e3eb160bf3e47be56ed11
허세진 <heosejin222@gmail.com>
2025-08-02 18:21:34 +0900

    style: 불필요한 import 제거

--- src/main/java/Majorpiece/bringstock/global/error/ErrorResponse.java
+++ src/main/java/Majorpiece/bringstock/global/error/ErrorResponse.java
@@ -1,8 +1,6 @@
 package Majorpiece.bringstock.global.error;
 
 import Majorpiece.bringstock.global.error.exception.ErrorCode;
-import lombok.Builder;
-import lombok.Getter;
 
 public record ErrorResponse (
 
