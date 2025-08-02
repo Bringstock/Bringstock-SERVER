536125a0d97c83d1ec7069469e5e1cb1514267f2
허세진 <heosejin222@gmail.com>
2025-08-02 18:20:20 +0900

    style: 공백 정리

--- src/main/java/Majorpiece/bringstock/global/error/exception/ErrorCode.java
+++ src/main/java/Majorpiece/bringstock/global/error/exception/ErrorCode.java
@@ -8,18 +8,18 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     // ===== JWT =====
-    EXPIRED_JWT_TOKEN(401,"JWT-401-1","Expired JWT Token"),
-    INVALID_JWT_TOKEN(401, "JWT-401-2","Invalid JWT Token"),
+    EXPIRED_JWT_TOKEN(401, "JWT-401-1", "Expired JWT Token"),
+    INVALID_JWT_TOKEN(401, "JWT-401-2", "Invalid JWT Token"),
 
     // ===== USER =====
-    USER_NOT_FOUND(404,"USER-404-1" ,"User Not Found"),
-    DUPLICATE_USER(409, "USER-409-1","Duplicate User"),
+    USER_NOT_FOUND(404, "USER-404-1","User Not Found"),
+    DUPLICATE_USER(409, "USER-409-1", "Duplicate User"),
 
     // ===== AUTH =====
-    LOGIN_FAILED(401, "AUTH-401-1","Invalid username or password"),
+    LOGIN_FAILED(401, "AUTH-401-1", "Invalid username or password"),
 
     // ===== SERVER =====
-    INTERNAL_SERVER_ERROR(500, "SERVER-500-1","Internal server error");
+    INTERNAL_SERVER_ERROR(500, "SERVER-500-1", "Internal server error");
 
     private final int status;
     private final String code;
