1ab1338a615d1447eb5130bec60ce0d5f08924ec
허세진 <heosejin222@gmail.com>
2025-08-02 17:21:22 +0900

    feat: 회원가입 및 로그인 요청 DTO에 유효성 검사 추가

--- src/main/java/Majorpiece/bringstock/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/Majorpiece/bringstock/domain/auth/dto/request/SignupRequest.java
@@ -1,7 +1,19 @@
 package Majorpiece.bringstock.domain.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record SignupRequest (
-    String username,
-    String password,
-    String email
+        @Size(min = 1, max = 15, message = "아이디는 최소 1자 이상, 최대 15자 이하여야 합니다.")
+        @NotBlank(message = "아이디는 필수입니다.")
+        String username,
+
+        @Size(min = 8,  max = 20, message = "비밀번호는 최소 8자 이상, 최대 20자 이하여야 합니다.")
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        String password,
+
+        @Size(min = 11, max = 255, message = "이메일은 최소 11자 이상, 최대 255자 이하여야 합니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email
 ){}
